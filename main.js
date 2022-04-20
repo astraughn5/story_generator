370dbbdf7c8eea932da0f3a616059da837b61485
astraughn5 <astraughn5@gmail.com>
2022-04-20 17:21:35 -0500

    added steps to main.js

--- main.js
+++ main.js
@@ -17,7 +17,11 @@ function randomValueFromArray(array){
 // Store the second set of three strings inside an array called insertY.
 // Store the third set of three strings inside an array called insertZ.
 */
+let storyText = `It was 94 fahrenheit outside, so ${insertXStory} went for a walk. When they got to ${insertYStory}, they stared in horror for a few moments, then ${insertZStory}. ${theName} saw the whole thing, but was not surprised — ${insertXStory} weighs 300 pounds, and it was a hot day.`
 
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
+let insertY = ['the soup kitchen','Disneyland', 'the White House']
+let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away']
 
 
 // Placing the event handler and incomplete function:
@@ -25,8 +29,26 @@ function randomValueFromArray(array){
 // Copy the code found underneath the heading "3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION" and paste it into the bottom of your main.js file. This:
 // Adds a click event listener to the randomize variable so that when the button it represents is clicked, the result() function is run.
 // Adds a partially-completed result() function definition to your code. For the remainder of the assessment, you'll be filling in lines inside this function to complete it and make it work properly.
-// Completing the result() function:
 
+function result() {
+
+	if(customName.value !== '') {
+	  const name = customName.value;
+  
+	}
+  
+	if(document.getElementById("uk").checked) {
+	  const weight = Math.round(300);
+	  const temperature =  Math.round(94);
+  
+	}
+  
+	// story.textContent = ;
+	story.style.visibility = 'visible';
+  }
+
+
+// Completing the result() function:
 // Create a new variable called newStory, and set its value to equal storyText. This is needed so we can create a new random story each time the button is pressed and the function is run. If we made changes directly to storyText, we'd only be able to generate a new story once.
 // Create three new variables called xItem, yItem, and zItem, and make them equal to the result of calling randomValueFromArray() on your three arrays (the result in each case will be a random item out of each array it is called on). For example you can call the function and get it to return one random string out of insertX by writing randomValueFromArray(insertX).
 // Next we want to replace the three placeholders in the newStory string — :insertx:, :inserty:, and :insertz: — with the strings stored in xItem, yItem, and zItem. There are two possible string methods that will help you here — in each case, make the call to the method equal to newStory, so each time it is called, newStory is made equal to itself, but with substitutions made. So each time the button is pressed, these placeholders are each replaced with a random silly string. As a further hint, depending on the method you choose, you might need to make one of the calls twice.
